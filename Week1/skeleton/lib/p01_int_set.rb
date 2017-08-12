f7de4d8157586bc5dc5d5becc402ccacb6db32fb
Sheri Wilson <smgwilson37@gmail.com>
2017-08-11 20:25:49 -0700

    implemented IntSet

--- Week1/skeleton/lib/p01_int_set.rb
+++ Week1/skeleton/lib/p01_int_set.rb
@@ -5,7 +5,7 @@ class MaxIntSet
 
   def insert(num)
     validate!(num)
-    return false if @store[num]
+    return false if include?(num)
     @store[num] = true
   end
 
@@ -39,18 +39,23 @@ class IntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
