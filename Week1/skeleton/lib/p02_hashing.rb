971046da63cbbd4e6348ef5b47303110f1982a92
Sheri Wilson <smgwilson37@gmail.com>
2017-08-12 11:30:32 -0700

    Implements hashing functions

--- Week1/skeleton/lib/p02_hashing.rb
+++ Week1/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+    (el.hash + i.hash) ^ intermediate_hash
+    end
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +20,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
