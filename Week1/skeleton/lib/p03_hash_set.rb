a15f70cc5b2a73cf3b2c038ae53683b800edca10
Sheri Wilson <smgwilson37@gmail.com>
2017-08-12 11:54:40 -0700

    Implemented HashSet

--- Week1/skeleton/lib/p03_hash_set.rb
+++ Week1/skeleton/lib/p03_hash_set.rb
@@ -9,18 +9,31 @@ class HashSet
   end
 
   def insert(key)
-  end
+    return false if include?(key)
+    self[key.hash] << key
+
+    @count += 1
+
+    if num_buckets < @count
+      resize!
+    end
+
+    key
+  end 
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +41,10 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.flatten.each {|k| insert(k)}
   end
 end
