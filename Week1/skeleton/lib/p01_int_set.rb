bce1bd20a6253c9e4b4a7bcc70cd994abead0e89
Sheri Wilson <smgwilson37@gmail.com>
2017-08-11 21:46:32 -0700

    implemented ResizingIntSet

--- Week1/skeleton/lib/p01_int_set.rb
+++ Week1/skeleton/lib/p01_int_set.rb
@@ -72,18 +72,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+
+    if num_buckets < @count
+      resize!
+    end
+
+    num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -91,5 +103,10 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.flatten.each {|num| insert(num)}
   end
 end
